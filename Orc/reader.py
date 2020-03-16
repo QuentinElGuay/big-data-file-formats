e66dc69eadd93402e38584e582b2be6f1ba00f62
Quentin <quentinleguay@laposte.net>
2020-03-16 22:53:43 +0100

    ORC reader

--- Orc/reader.py
+++ Orc/reader.py
@@ -1,9 +1,30 @@
-import pyorc
+from pyorc import Reader
+from pyorc.enums import StructRepr
 
 ORC_FILE = 'data/orc/userdata1.orc'
 
 with open(ORC_FILE, 'rb') as orc_file:
-    reader = pyorc.Reader(orc_file)
-
+    reader = Reader(orc_file)
+    
+    # Read embedded schema
     print(str(reader.schema))
-    #print(reader.read())
+    
+    # Read all the file at once:
+    rows = reader.read()
+    print(rows)
+
+    # Go back to first line
+    reader.seek(0)
+
+    # Read the content of userdata1.orc by batch of 100 records
+    # Using this optional parameter for large ORC files is highly recommended!
+    rows = reader.read(100)
+    while rows:
+        print(rows)
+        rows = reader.read(100)
+
+    # Read file and return a list of dictionaries
+    reader = Reader(orc_file, struct_repr=StructRepr.DICT)
+    print(next(reader))
+
+    print(reader.num_of_stripes)
